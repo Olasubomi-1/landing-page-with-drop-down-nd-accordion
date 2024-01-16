47b192758e06103b88216fc7f2ef166f07d67825
Olasubomi-1 <afolayanolayimika2017@gmail.com>
2024-01-16 12:09:34 +0100

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+let submenu = document.getElementById("sub-menu");
+
+function toggleMenu() {
+  submenu.classList.toggle("open-menu");
+}
+
+let reveal = document.getElementById("reveal");
+
+function toggleMenuy() {
+  reveal.classList.toggle("open-list");
+}
